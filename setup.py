9d8e6a5a48809b5f8bd56df9cefd34341e1cc07b
KFLMIAMI420 <42599019+kflmiami420@users.noreply.github.com>
2019-09-19 15:17:32 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,27 +1,31 @@
-from ez_setup import use_setuptools
-use_setuptools()
-from setuptools import setup, find_packages, Extension
-import sys
+import os
+import io
+from setuptools import setup, find_packages
 
+here = os.path.abspath(os.path.dirname(__file__))
 
-# Check if an explicit platform was chosen with a command line parameter.
-# Kind of hacky to manipulate the argument list before calling setup, but it's
-# the best simple option for adding optional config to the setup.
+# Import the README and use it as the long-description.
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
 
-# Pick the right extension to compile based on the platform.
-extensions = []
-extensions.append(Extension("Adafruit_DHT.Raspberry_Pi_Driver", 
-     ["source/_Raspberry_Pi_Driver.c", "source/common_dht_read.c", "source/Raspberry_Pi/pi_dht_read.c", "source/Raspberry_Pi/pi_mmio.c"], 
-     libraries=['rt'],
-     extra_compile_args=['-std=gnu99']))
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: MIT License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
 
-# Call setuptools setup function to install package.
-setup(name              = 'Adafruit_DHT',
-  version           = '1.0.0',
-  author            = 'Tony DiCola',
-  author_email      = 'tdicola@adafruit.com',
-  description       = 'Library to get readings from the DHT11, DHT22, and AM2302 humidity and temperature sensors on a Raspberry Pi or Beaglebone Black.',
-  license           = 'MIT',
-  url               = 'https://github.com/adafruit/Adafruit_Python_DHT/',
-  packages          = find_packages(),
-  ext_modules       = extensions)
+setup(name              = 'Adafruit_SSD1306',
+      version           = '1.6.2',
+      author            = 'Tony DiCola',
+      author_email      = 'tdicola@adafruit.com',
+      description       = 'Python library to use SSD1306-based 128x64 or 128x32 pixel OLED displays with a Raspberry Pi or Beaglebone Black.',
+      long_description  = long_description,
+      license           = 'MIT',
+      classifiers       = classifiers,
+      url               = 'https://github.com/adafruit/Adafruit_Python_SSD1306/',
+      dependency_links  = ['https://github.com/adafruit/Adafruit_Python_GPIO/tarball/master#egg=Adafruit-GPIO-0.6.5'],
+      install_requires  = ['Adafruit-GPIO>=0.6.5'],
+      packages          = find_packages())
